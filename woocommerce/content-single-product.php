cf9c07a53cec5bee46c68337eaafe279b4f6cd1f
Joneel <real.joneel@gmail.com>
2018-08-23 07:03:54 +0800

    # Hide Pricing/Cart Button Single Product

---
+++ woocommerce/content-single-product.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * The template for displaying product content in the single-product.php template
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/content-single-product.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     3.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+global $product;
+$terms = has_term( 'parts-accessories', 'product_cat', get_the_ID() );
+
+/**
+ * Hook Woocommerce_before_single_product.
+ *
+ * @hooked wc_print_notices - 10
+ */
+do_action( 'woocommerce_before_single_product' );
+
+if ( post_password_required() ) {
+	echo get_the_password_form(); // WPCS: XSS ok.
+	return;
+}
+?>
+<div id="product-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<?php
+		/**
+		 * Hook: woocommerce_before_single_product_summary.
+		 *
+		 * @hooked woocommerce_show_product_sale_flash - 10
+		 * @hooked woocommerce_show_product_images - 20
+		 */
+		do_action( 'woocommerce_before_single_product_summary' );
+	?>
+
+	<div class="summary entry-summary<?php echo ($terms) ? ' summary-accessory' : ''; ?>">
+		<?php
+			/**
+			 * Hook: Woocommerce_single_product_summary.
+			 *
+			 * @hooked woocommerce_template_single_title - 5
+			 * @hooked woocommerce_template_single_rating - 10
+			 * @hooked woocommerce_template_single_price - 10
+			 * @hooked woocommerce_template_single_excerpt - 20
+			 * @hooked woocommerce_template_single_add_to_cart - 30
+			 * @hooked woocommerce_template_single_meta - 40
+			 * @hooked woocommerce_template_single_sharing - 50
+			 * @hooked WC_Structured_Data::generate_product_data() - 60
+			 */
+			do_action( 'woocommerce_single_product_summary' );
+		?>
+
+		<?php if(!$terms && $product->is_in_stock()) : ?>
+
+            <div class="col-lg-12">
+                <div itemprop="offers" class="product_price" itemscope itemtype="http://schema.org/Offer">
+
+                    <p class="price"><?php echo $product->get_price_html(); ?></p>
+
+                    <meta itemprop="price" content="<?php echo esc_attr( $product->get_price() ); ?>" />
+                    <meta itemprop="priceCurrency" content="<?php echo esc_attr( get_woocommerce_currency() ); ?>" />
+                    <link itemprop="availability" href="http://schema.org/<?php echo $product->is_in_stock() ? 'InStock' : 'OutOfStock'; ?>" />
+
+                </div>
+            </div>
+
+            <form class="cart" action="<?php echo esc_url( apply_filters( 'woocommerce_add_to_cart_form_action', $product->get_permalink() ) ); ?>" method="post" enctype='multipart/form-data'>
+				<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
+
+				<?php
+				do_action( 'woocommerce_before_add_to_cart_quantity' );
+
+				woocommerce_quantity_input( array(
+					'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
+					'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
+					'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( wp_unslash( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
+				) );
+
+				do_action( 'woocommerce_after_add_to_cart_quantity' );
+				?>
+
+                <button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button button alt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+
+				<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
+            </form>
+
+			<?php do_action( 'woocommerce_after_add_to_cart_form' ); ?>
+
+		<?php endif; ?>
+
+	</div>
+
+</div>
+
+
+
